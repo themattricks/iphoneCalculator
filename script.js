c3f6bf30726e62139732801347437a51b6f9ddef
Omar El Sayed <90359498+themattricks@users.noreply.github.com>
2022-02-01 23:09:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 var numberValue = document.querySelectorAll(".container .row .primaryButton");
+var GonnaBeDecimal = 0;
 for (var i = 0; i < numberValue.length; i++) {
     numberValue[i].addEventListener('click', function() {
         // console.log("You clicked:", this.innerHTML);
+        // If You Didn't Get The Operation Yet
+        // Means That You Gonna Take The First Number
         if (typeof(basicOperation) !== 'string') {
+            // If There's A Number Already Add The Clicked Number To It
             if (typeof(firstNumber) == "number") {
-                firstNumber = parseInt(firstNumber.toString() + this.innerHTML.toString());
+                if (this.innerHTML.toString() == '.') {
+                    firstNumber = parseFloat(firstNumber.toString() + this.innerHTML.toString());
+                    GonnaBeDecimal = 1;
+                } else {
+                    if (GonnaBeDecimal == 1) {
+                        firstNumber = parseFloat(firstNumber.toString() + '.' + this.innerHTML.toString());
+                    } else {
+                        GonnaBeDecimal = 0;
+                        firstNumber = parseInt(firstNumber.toString() + this.innerHTML.toString());
+                    }
+                }
+            // If There's Not Just Get The Clicked Number
             } else {
                 firstNumber = parseInt(this.innerHTML);
             }
+            // Show The Number In The Result Area
             document.getElementById('result').innerHTML = firstNumber;
+        // If There Is Already An Operation
         } else {
             if (typeof(secondNumber) == "number") {
                 secondNumber = parseInt(secondNumber.toString() + this.innerHTML.toString());
@@ -28,7 +45,14 @@ for (var i = 0; i < operation.length; i++) {
         operations.push(basicOperation);
         if (basicOperation == "=") {
             var operationResult = eval((firstNumber) + operations[operations.length - 2] + (secondNumber));
-            document.getElementById('result').innerHTML = operationResult;
+            operationResult = parseFloat(operationResult).toString();
+            if (operationResult.length > 5) {
+                operationResult = parseFloat(operationResult).toFixed(4);
+            }
+            document.getElementById('result').innerHTML = parseFloat(operationResult);
+            firstNumber = operationResult;
+            delete secondNumber;
+            delete basicOperation;
         }
         //document.getElementById('result').innerHTML = parseInt(this.innerHTML);
     });
